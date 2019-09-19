c6bf8e7fd651a3ea577bd6279873c2d109c3ec13
ishuram <ishuram@gmail.com>
2019-09-19 12:39:07 -0400

    rectangle.cs
    adding rectangle class commit

---
+++ Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1_Seng8040
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+
+        public int GetPerimeter()
+        {
+            return (2 * (length + width));
+        }
+
+        public int GetArea()
+        {
+            return (length * width);
+        }
+
+    }
+}
+
+
